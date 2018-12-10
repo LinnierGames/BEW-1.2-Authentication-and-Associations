3308f6a298706eabf29486f15af484c4b3d38c72
LinnierGames <esericksanc@gmail.com>
2018-12-09 18:46:26 -0800

    Create a Post

---
+++ Projects/redit-clone/redit-clone/routes/posts.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const Posts = require('../models/posts')
+
+/* GET users listing. */
+router.get('/new', function(req, res, next) {
+  res.render('posts-new', { title: "New Post" });
+});
+
+router.post('/new', function(req, res, next) {
+  console.log(req.body)
+  Posts.create(req.body, (error, newPost) => {
+    res.send((error, newPost))
+  })
+});
+
+module.exports = router;
